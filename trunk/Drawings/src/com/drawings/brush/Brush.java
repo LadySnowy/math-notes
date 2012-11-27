fea5c9d941f91d0fdd41912aa1766afdf134a48d
thesnowdragoon@gmail.com <thesnowdragoon@gmail.com@9efa182e-bf42-bef6-3135-144cb0842b06>
2012-11-27 05:01:13 +0000

    code cleaning of brushes

--- trunk/Drawings/src/com/drawings/brush/Brush.java
+++ trunk/Drawings/src/com/drawings/brush/Brush.java
@@ -2,13 +2,6 @@ package com.drawings.brush;
 
 import android.graphics.Path;
 
-/**
- * Created by IntelliJ IDEA.
- * User: almondmendoza
- * Date: 01/12/2010
- * Time: 10:54 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Brush implements  IBrush {
     public void mouseDown(Path path, float x, float y) {
         //To change body of implemented methods use File | Settings | File Templates.
